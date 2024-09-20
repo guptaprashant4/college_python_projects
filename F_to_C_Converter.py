8b8944ff584425f414ddb58c8f8d706d7b4c0af4
Prashant Gupta <93766389+guptaprashant4@users.noreply.github.com>
2024-09-20 15:32:37 -0500

    Add files via upload
    This file converts temperature in degree Fahrenheit to degree Celcius.

---
+++ F_to_C_Converter.py
@@ -0,0 +1,7 @@
+# File Name: celsius.py
+# Program Description: A program to convert Celsius temps to Fahrenheit
+
+print("This program will convert a Celsius temperature entered by the user to Fahrenheit.")
+celsius = eval(input("What is the Celcius temperature?"))
+fahrenheit = 9/5 * celsius + 32
+print("The temperature is", fahrenheit, "degrees Fahrenheit. ")
